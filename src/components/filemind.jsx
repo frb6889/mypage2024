d6501b04dc84ee1d7c2dba3fda30edaebb1ab24b
syr <sufr3b9@foxmail.com>
2024-03-31 21:42:47 +0800

    t

--- src/components/filemind.jsx
+++ src/components/filemind.jsx
@@ -1,38 +1,118 @@
 import React from 'react';
+import * as React from "react"
+import '../app/style.css';
+import 'tailwindcss/tailwind.css';
+import { Button } from "@/components/ui/button";
 
-const FileMind = () => {
-  const containerStyle = {
-    display: 'flex',
-    alignItems: 'center',
-    backgroundColor: 'rgb(223, 236, 221)',  // 设置浅蓝色背景
-    borderRadius:'1rem',
-    paddingTop: '2px',  // 添加一些内边距
-    paddingLeft: '10px',
-    paddingRight: '10px',
-    paddingBottom: '2px',  // 添加一些内边距
+import {
+    Carousel,
+    CarouselContent,
+    CarouselItem,
+    CarouselNext,
+    CarouselPrevious,
+} from "@/components/ui/carousel"
 
-    marginTop:'14px',
-    width: 'max-content',  // 设置容器的宽度为 max-content
-  };
 
-  const dotStyle = {
-    width: '8px',
-    height: '8px',
-    backgroundColor: 'rgb(118, 154, 127)',  // 设置蓝色圆点颜色
-    borderRadius: '50%',
-    marginRight: '8px',  // 设置圆点与文字之间的间距
-  };
-  const textStyle = {
-    fontSize: '13px',
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
 
-  };
+import { cn } from "@/lib/utils";
+import Image from "next/image"
+import { useRouter } from "next/navigation"
 
-  return (
-    <div style={containerStyle}>
-      <span style={dotStyle}></span>
-      <p style={textStyle}>Done</p>
-    </div>
-  );
+
+import { FaLinkedin } from 'react-icons/fa';
+import { AspectRatioDemo } from "@/components/apic"
+import { AspectRatio } from "../components/ui/aspect-ratio"
+import InProgressComponent from "@/components/inprogress"
+
+
+export function FileMind() {
+    const router = useRouter();
+
+    const toHome = () => {
+        router.push('/');
+    }
+
+    const CircularCard = ({ children }) => (
+        <div className="circular-card" style={{ width: '230px', height: '230px', backgroundColor: 'rgba(47, 114, 241, .1)', borderRadius: '50%', margin: '-10px' }}>
+            <CardHeader>
+                <CardDescription style={{ marginTop: '28%', fontSize: '20px', marginLeft: '20px' }}>
+                    {children}
+                </CardDescription>
+            </CardHeader>
+        </div>
+    );
+
+    const PointCard = ({ children }) => (
+        <Card style={{ width: '180px', marginBottom: '20px' }}>
+            <CardHeader>
+                <CardTitle className="flex justify-center">
+                    {children}
+                </CardTitle>
+            </CardHeader>
+        </Card>
+    )
+
+    const PainpointCard = ({ children }) => (
+        <div className="painpoint-card">
+            {children}
+        </div>
+    );
+
+
+    return (
+        <main className="px-24 py-2" style={{ color: 'rgb(3, 8, 22)', backgroundColor: 'babyblue' }}>
+
+            {/* page */}
+            <section className="py-1 px-10 flex flex-col item-center text-left gap-3">
+                <h1 className="scroll-m-20 font-extrabold  lg:text-6xl" style={{ marginTop: '80px' }}>
+                    Filemind</h1>
+                <div>
+                    <div className="flex justify-center item-center gap-6" style={{ marginTop: '20px' }}>
+                        <Card className="w-[350px]">
+                            <CardHeader>
+                                <CardTitle>Working Process</CardTitle>
+                                <CardDescription>Started In November 2023
+                                    <br />Lasted for 10 Months</CardDescription>
+                            </CardHeader>
+                        </Card>
+                        <Card className="w-[350px]">
+                            <CardHeader>
+                                <CardTitle>Our Team</CardTitle>
+                                <CardDescription>5 Developers
+                                    <br />1 Mentor
+                                    <br />1 Designer(me)</CardDescription>
+                            </CardHeader>
+                        </Card>
+                        <Card className="w-[350px]">
+                            <CardHeader>
+                                <CardTitle>My Role As Design Lead</CardTitle>
+                                <CardDescription>
+                                    User Research And Interviews
+                                    <br />Reviewed Relevant Papers
+                                    <br />Low/high-fidelity Prototypes
+                                    <br />Frontend Development
+                                </CardDescription>
+                            </CardHeader>
+                        </Card>
+
+                    </div>
+                </div>
+                <Image src="./filemind_research.png" alt="filemind-research" width={600} />
+
+
+            </section>
+
+
+        </main>
+    );
 };
 
 export default FileMind;
