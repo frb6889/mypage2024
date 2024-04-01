4a3c8a64126ca75b5e68fe9c7a2dc6b135fe7b56
syr <sufr3b9@foxmail.com>
2024-04-01 08:31:41 +0800

    r?

--- src/app/page.js
+++ src/app/page.js
@@ -1,25 +1,29 @@
-'use client'
-
-import * as React from "react"
-import { useState } from "react"
-
-import Image from "next/image"
-//import { useNavigation } from "next/navigation"
-import { useRouter } from "next/navigation"
-import { Button } from "@/components/ui/button"
+import * as React from "react";
+import { useState, useEffect } from "react";
+import Image from "next/image";
 import { FaLinkedin } from 'react-icons/fa';
-import { AspectRatioDemo } from "@/components/apic"
+import { useRouter } from "next/router";
+import { AspectRatioDemo } from "@/components/apic";
 import './style.css';
-import { AspectRatio } from "@/components/ui/aspect-ratio"
-import InProgressComponent from "@/components/inprogress"
-import DoneComponent from "@/components/done"
-import FileMind from "@/components/filemind"
-import InHunt from "@/components/inhunt"
-
-
+import { AspectRatio } from "@/components/ui/aspect-ratio";
+import InProgressComponent from "@/components/inprogress";
+import DoneComponent from "@/components/done";
+import FileMind from "@/components/filemind";
+import InHunt from "@/components/inhunt";
 
 export default function Home() {
   const router = useRouter();
+  const [showReview, setShowReview] = useState(false);
+  const [selectedComponent, setSelectedComponent] = useState(null);
+
+  useEffect(() => {
+    function handleResize() {
+      setIsMobile(window.innerWidth < 768); // Adjust the breakpoint as per your design
+    }
+    window.addEventListener('resize', handleResize);
+    handleResize(); // Initial check
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
 
   const toLinkedIn = () => {
     const linkedInUrl = "https://www.linkedin.com/in/yuran-su-6b96a2281/";
@@ -31,159 +35,55 @@ export default function Home() {
     window.open(PIurl, '_blank');
   }
 
-  const [showReview, setShowReview] = React.useState(false);
-  const [selectedComponent, setSelectedComponent] = React.useState(null);
-
-
-  // 点击表格中的 "accounting" 单元格时，显示新的界面
   const handleClick = (component) => {
-    setShowReview(true); // Set showReview to true to display review section
-    setSelectedComponent(component); // Set the selected component
+    setShowReview(true);
+    setSelectedComponent(component);
   };
 
-  const [open, setOpen] = React.useState(false)
-
-
-
   return (
-
-    <main className="px-24 py-2">
+    <main className="px-2 md:px-24 py-2">
       {!showReview ? (
         <div className="mainpage">
-          <header className="flex justify-between items-center"
+          <header className="flex justify-between items-center md:px-24"
             style={{
               position: 'fixed', border: '0.4px solid #dddddd',
-              width: '80%', margin: 0, padding: 10, paddingLeft: 40, paddingRight: 40, zIndex: 1000, borderRadius: '10px',
+              width: '100%', margin: 0, padding: 10, zIndex: 1000, borderRadius: '10px',
               backdropFilter: 'blur(8px)', backgroundColor: 'rgba(255, 255, 255, 0.5)'
             }}>
             <div className="flex items-center">
               <Image
                 src="./head.png" alt="Logo" width={60} height={60} />
             </div>
-
-
             <div className="flex gap-8">
-
-             {/*  <Button>Resume</Button>
-              <Button>About</Button> */}
-
               <button onClick={toLinkedIn} className="flex items-center gap-2">
                 <FaLinkedin size={20} />
               </button>
             </div>
           </header>
 
-          {/* my info */}
           <section className="py-24 px-10 flex flex-col item-center text-left gap-3">
-            <h1 className="scroll-m-20 text-4xl font-extrabold  lg:text-4xl" style={{ marginTop: '60px' }}>Erana(Yuran) Su</h1>
-            <p className="text-2x1 text-muted-foreground">Product Designer@THU IAD</p>
+            <h1 className="scroll-m-20 text-4xl font-extrabold md:text-4xl" style={{ marginTop: '60px' }}>Erana(Yuran) Su</h1>
+            <p className="text-2xl text-muted-foreground">Product Designer@THU IAD</p>
           </section>
 
-          {/* filemind */}
-          <section className="px-14 gap-8 flex items-center py-10">
-            <div className="Container">
-              <AspectRatioDemo />
-            </div>
-            <div className="Container flex-col gap-10 items-left">
-
-              <h1 className="scroll-m-20 text-2xl font-bold  lg:text-3xl">FileMind</h1>
-              <InProgressComponent />
-              <p className="text-muted-foreground lg:text-2l" style={{ marginTop: '7px' }}>#Landing Project
-              </p>
-              <p onClick={toPIgroupWebsite} className="text-muted-foreground lg:text-2l" style={{ cursor: 'pointer' }}>#Tsinghua University Pervasive HCI Group</p>
-              <h1 className="scroll-m-20 text-2l text-primary" style={{ marginTop: '2px' }}>
-                A Data Management Assistant tailored for financial systems,
-                streamlining the organization and control of data tables for enhanced efficiency.
-              </h1>
-              <Button style={{ marginTop: '20px' }} onClick={() => handleClick('FileMind')}>Click to see more</Button>
-            </div>
+          <section className="px-14 gap-8 flex flex-col md:flex-row items-center py-10">
+            {/* Content for FileMind */}
           </section>
 
-          {/* inhunt */}
-          <section className="px-14 gap-8 flex items-center py-10">
-            <div className="Container">
-              <AspectRatio ratio={4 / 3} className=" w-30">
-                <Image
-                  //src="https://images.unsplash.com/photo-1588345921523-c2dcdb7f1dcd?w=800&dpr=2&q=80"
-                  src="./inhunt.png"
-                  alt="Photo by Drew Beamer"
-                  fill
-                  className="rounded-md object-contain"
-                />
-              </AspectRatio>
-            </div>
-            <div className="Container flex-col gap-10 items-left">
-
-              <h1 className="scroll-m-20 text-2xl font-bold  lg:text-3xl">Inhunt</h1>
-              <DoneComponent />
-              <p className="text-muted-foreground lg:text-2l" style={{ marginTop: '7px' }}>#Collaboration With Alibaba
-              </p>
-              <h1 className="scroll-m-20 text-2l text-primary" style={{ marginTop: '2px' }}>
-              InHunt is an app designed specifically for online novel authors, serving as an inspiration capturing and organizing assistant.
-              </h1>
-              <Button sclassName="inhuntButton" onClick={() => handleClick(InHunt)} style={{ marginTop: '20px' }}>Click to see more</Button>
-            </div>
+          <section className="px-14 gap-8 flex flex-col md:flex-row items-center py-10">
+            {/* Content for Inhunt */}
           </section>
 
-          {/* eno */}
-          <section className="px-14 gap-8 flex items-center py-10">
-            <div className="Container">
-              <AspectRatio ratio={4 / 3} className=" w-30">
-                <Image
-                  //src="https://images.unsplash.com/photo-1588345921523-c2dcdb7f1dcd?w=800&dpr=2&q=80"
-                  src="./eno.png"
-                  alt="Photo by Drew Beamer"
-                  fill
-                  className="rounded-md object-contain"
-                />
-              </AspectRatio>
-            </div>
-            <div className="Container flex-col gap-10 items-left">
-
-              <h1 className="scroll-m-20 text-2xl font-bold  lg:text-3xl">Eno</h1>
-              <DoneComponent />
-              <p className="text-muted-foreground lg:text-2l" style={{ marginTop: '7px' }}>#Collaboration With Alibaba
-              </p>
-              <h1 className="scroll-m-20 text-2l text-primary" style={{ marginTop: '2px' }}>
-                An app for gen-z mental problems.
-              </h1>
-              <Button variant="secondary" style={{ marginTop: '20px' }}>still working...</Button>
-            </div>
+          <section className="px-14 gap-8 flex flex-col md:flex-row items-center py-10">
+            {/* Content for Eno */}
           </section>
 
           <section className="py-10 px-14 gap-2 flex items-center">
-            <div style={{ width: 200 + 'px', }}>
-              <AspectRatio ratio={4 / 3} className=" w-30" >
-                <Image
-                  //src="https://images.unsplash.com/photo-1588345921523-c2dcdb7f1dcd?w=800&dpr=2&q=80"
-                  src="./head.png"
-                  alt="Photo by Drew Beamer"
-                  width={300}
-                  height={200}
-                  className="rounded-md object-contain"
-                />
-              </AspectRatio>
-            </div>
-            <div className="py-24 px-10 flex flex-col item-center text-left gap-3">
-              <h1 className="scroll-m-20 text-4xl font-extrabold  lg:text-2xl">Hi,I am Erana.</h1>
-              <p className="text-2x1 text-muted-foreground">
-                Since I am a non-binary, you can also call me Eran.
-                <br />
-                I am a student at Tsinghua University, studying Information Art and Design.
-                <br />
-                I got into design and coding because I wanted to create tools that actually make life easier.
-                Over time, this has also become a way for my self-expression.
-                <br />
-                Outside of design, I love drinking tea, camping, and writing.
-                <br />
-                Oolong tea and Earl Grey are my go-to favorites.
-              </p>
-            </div>
-
+            {/* About me section */}
           </section>
-        </div>) : (
+        </div>
+      ) : (
         <div style={{ color: 'rgb(3, 8, 22)', backgroundColor: 'babyblue' }}>
-          {/* 返回键 */}
           <header className="flex justify-between items-center"
             style={{
               position: 'fixed', border: '0.4px solid #dddddd',
