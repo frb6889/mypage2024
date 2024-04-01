9dca140392dbf52acfddaa3d70cd60f8e0d6936b
syr <sufr3b9@foxmail.com>
2024-04-01 09:00:57 +0800

    r4

--- src/components/apic.jsx
+++ src/components/apic.jsx
@@ -4,7 +4,7 @@ import { AspectRatio } from "@/components/ui/aspect-ratio"
 
 export function AspectRatioDemo() {
   return (
-    <AspectRatio ratio={4 / 3} className=" w-30">
+    <AspectRatio ratio={4 / 3} style={{width: '80%', marginLeft: '10%',marginRight: '10%'}}>
       <Image
       //src="https://images.unsplash.com/photo-1588345921523-c2dcdb7f1dcd?w=800&dpr=2&q=80"
         src="./filemind.png"
