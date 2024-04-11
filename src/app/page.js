ea37b77293669134206231801fa6c7c2a4457500
syr <sufr3b9@foxmail.com>
2024-04-11 23:40:29 +0800

    0411

--- src/app/page.js
+++ src/app/page.js
@@ -88,7 +88,8 @@ export default function Home() {
 
             <div className="h-300"></div>
           {/* my info */}
-          <div style={{height:'40%' , backgroundColor:'red'}}>
+          <div style={{height:'200px' , backgroundColor:'green'}}></div>
+          <div style={{height:'40%' , backgroundColor:'red',margin:'30px'}}>
           <section className="max-w-md mx-auto my-10 flex-col justify-between items-center" style={{marginTop:'50px',marginBottom:'50px'}}>
                   <h1 className="text-4xl font-extrabold" style={{ marginTop: '100px' }}>Erana(Yuran) Su</h1>
                   <p className="text-2x1 text-muted-foreground">Product Designer@THU IAD</p>
