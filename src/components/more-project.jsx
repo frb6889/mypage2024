e34238af026ac59502f83e19f2ad65e36ec9f2c9
syr <sufr3b9@foxmail.com>
2024-04-12 10:54:35 +0800

    0412

--- src/components/more-project.jsx
+++ src/components/more-project.jsx
@@ -9,16 +9,16 @@ import { Button } from "@/components/ui/button";
 const MoreProject = ({ onClickDir }) => {
   const works = [
     {
-      artist: "Ornella Binni",
-      art: "https://images.unsplash.com/photo-1465869185982-5a1a7522cbcb?auto=format&fit=crop&w=300&q=80",
+      artist: "Slow Homecoming",
+      art: "./slow_homecoming.png",
     },
     {
-      artist: "Tom Byrom",
-      art: "https://images.unsplash.com/photo-1548516173-3cabfa4607e9?auto=format&fit=crop&w=300&q=80",
+      artist: "Lume",
+      art: "./lume.png",
     },
     {
-      artist: "Vladimir Malyavko",
-      art: "https://images.unsplash.com/photo-1494337480532-3725c85fd2ab?auto=format&fit=crop&w=300&q=80",
+      artist: "Fragmentation",
+      art: "./Fragmentation.png",
     },
   ]
 
