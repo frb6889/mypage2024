8cfcafd6f187dec9f1cd91b4e2740757ec2ffdf3
syr <sufr3b9@foxmail.com>
2024-04-12 11:03:05 +0800

    0412

--- src/components/more-project.jsx
+++ src/components/more-project.jsx
@@ -23,37 +23,40 @@ const MoreProject = ({ onClickDir }) => {
   ]
 
   return (
-    
-    
-    
-    <ScrollArea className="max-w-md mx-auto bg-white md:max-w-2xl whitespace-nowrap rounded-md border">
-      <div className="flex w-max space-x-4 p-4">
+
+
+    <div className="max-w-md mx-auto bg-white md:max-w-2xl whitespace-nowrap">
       <a class="block mt-1 leading-tight text-black scroll-m-20 font-extrabold text-2xl">More project</a>
-        {works.map((artwork) => (
-          <figure key={artwork.artist} className="shrink-0">
-            <div className="overflow-hidden rounded-md">
-              <Image
-                src={artwork.art}
-                alt={`Photo by ${artwork.artist}`}
-                className="aspect-[3/4] h-fit w-fit object-cover"
-                width={300}
-                height={400}
-              />
-            </div>
-            <figcaption className="pt-2 text-xs text-muted-foreground">
-              Photo by{" "}
-              <span className="font-semibold text-foreground">
-                {artwork.artist}
-              </span>
-              <Button style={{ marginTop: '20px' }} onClick={onClickDir}>
-                Click to see more
-              </Button>
-            </figcaption>
-          </figure>
-        ))}
-      </div>
-      <ScrollBar orientation="horizontal" />
-    </ScrollArea>
+      <ScrollArea className="max-w-md mx-auto bg-white md:max-w-2xl whitespace-nowrap rounded-md border">
+
+        <div className="flex w-max space-x-4 p-4">
+          {works.map((artwork) => (
+            <figure key={artwork.artist} className="shrink-0">
+              <div className="overflow-hidden rounded-md">
+                <Image
+                  src={artwork.art}
+                  alt={`Photo by ${artwork.artist}`}
+                  className="aspect-[3/4] h-fit w-fit object-cover"
+                  width={300}
+                  height={400}
+                />
+              </div>
+              <figcaption className="pt-2 text-xs text-muted-foreground">
+                Photo by{" "}
+                <span className="font-semibold text-foreground">
+                  {artwork.artist}
+                </span>
+                <Button style={{ marginTop: '20px' }} onClick={onClickDir}>
+                  Click to see more
+                </Button>
+              </figcaption>
+            </figure>
+          ))}
+        </div>
+        <ScrollBar orientation="horizontal" />
+      </ScrollArea>
+    </div>
+
   )
 }
 
