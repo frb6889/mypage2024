66e557bf1cc492071e2abe338e65efe023fe7afe
syr <sufr3b9@foxmail.com>
2024-04-12 12:32:40 +0800

    10

--- src/components/lume.jsx
+++ src/components/lume.jsx
@@ -25,7 +25,6 @@ import {
 import { cn } from "@/lib/utils";
 import Image from "next/image"
 import { useRouter } from "next/navigation"
-import { Button } from "@/components/ui/button";
 
 
 import { FaLinkedin } from 'react-icons/fa';
