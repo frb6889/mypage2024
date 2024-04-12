4a041f584011f2d9db350aaac5908efaccdd6fcb
syr <sufr3b9@foxmail.com>
2024-04-12 12:02:11 +0800

    wtf

--- src/components/more-project.jsx
+++ src/components/more-project.jsx
@@ -11,17 +11,17 @@ const MoreProject = ({dir1,dir2,dir3}) => {
     {
       name: "Slow Homecoming",
       urll: "./slow_homecoming.png",
-      
+      onClickDir : dir1,
     },
     {
       name: "Lume",
       urll: "./lume.png",
-     
+      onClickDir : dir2,
     },
     {
       name: "Fragmentation",
       urll: "./Fragmentation.png",
-      
+      onClickDir : dir3,
     },
   ]
 
@@ -31,10 +31,10 @@ const MoreProject = ({dir1,dir2,dir3}) => {
     <div className="max-w-md mx-auto bg-white md:max-w-2xl whitespace-nowrap">
       <a class="block mt-1 leading-tight text-black scroll-m-20 font-extrabold text-2xl"
       style={{margin:'20px'}}>More project</a>
-      <ScrollArea className="max-w-md mx-auto bg-white md:max-w-2xl whitespace-nowrap">
+      <ScrollArea className="max-w-md mx-auto bg-white md:max-w-2xl whitespace-nowrap rounded-md border">
 
         <div className="flex w-max space-x-4 p-4">
-          {/* {works.map((artwork) => (
+          {works.map((artwork) => (
             <figure key={artwork.name} className="shrink-0">
               <div className="overflow-hidden rounded-md">
                 <Image
@@ -45,29 +45,13 @@ const MoreProject = ({dir1,dir2,dir3}) => {
                 />
               </div>
               <figcaption className="pt-2 text-xs text-muted-foreground gap-3">
-                
+                {/* Photo by{" "} */}
                 <Button variant="outline" style={{ height:"30px" }} onClick={artwork.onClickDir}>
                 {artwork.name}
                 </Button>
               </figcaption>
             </figure>
-          ))} */}
-          <figure key="Slow Homecoming" className="shrink-0">
-              <div className="overflow-hidden rounded-md">
-                <Image
-                  src="./slow_homecoming.png"
-                  className="aspect-[4/3] h-fit w-fit object-cover"
-                  width={300}
-                  height={400}
-                />
-              </div>
-              <figcaption className="pt-2 text-xs text-muted-foreground gap-3">
-                
-                <Button variant="outline" style={{ height:"30px" }} onClick={artwork.onClickDir}>
-                {artwork.name}
-                </Button>
-              </figcaption>
-            </figure>
+          ))}
         </div>
         <ScrollBar orientation="horizontal" />
       </ScrollArea>
